fdbf88999acb244ab8c46b1285f178fb37c6d83c
WhyTry313 <fx.schindel@gmail.com>
2024-03-11 05:58:38 +0100

    [UPDATE] Adds fileExists method

--- Utils/utils.js
+++ Utils/utils.js
@@ -1,13 +1,15 @@
 const File = new godot.File();
-
+const file_exists = (path) => File.file_exists(path);
 export const log = (txt, strTab, indent) => {
 	return console.log(JSON.stringify(txt, strTab, indent));
 };
 
 export const getFile = (path) => {
-    if (!File.file_exists(path)) { throw new Error("Couldn't find "+path); }
+    if (!file_exists(path)) { throw new Error("Couldn't find "+path); }
     File.open(path, godot.File.READ);
     const text = File.get_as_text();
     File.close();
     return text;
 };
+
+export const fileExists = file_exists;
